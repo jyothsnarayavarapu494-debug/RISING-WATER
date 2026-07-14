3431aef1d1fa58e8fbed8d7b33bd2344db6e68b7
jyothsnarayavarapu494-debug <jyothsnarayavarapu494@gmail.com>
2026-07-14 20:08:36 +0530

    Add files via upload

---
+++ imposting the libraries task 1.py
@@ -0,0 +1,30 @@
+# ================================
+# Rising Waters - Import Libraries
+# ================================
+
+# Numerical computing
+import numpy as np
+
+# Data analysis
+import pandas as pd
+
+# Data visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Machine Learning
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+
+# XGBoost
+# from xgboost import XGBClassifier
+
+# Flask
+# from flask import Flask
+
+# Save and load models
+import joblib
+import pickle
+
+print("All libraries imported successfully!")
