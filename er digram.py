ee8ddbb225886bf88a268739dd90d1000ae3486d
jyothsnarayavarapu494-debug <jyothsnarayavarapu494@gmail.com>
2026-07-15 12:40:19 +0530

    Add files via upload

---
+++ er digram.py
@@ -0,0 +1,20 @@
+
+print("Building HTML Pages Task")
+
+print("""
+Files Required:
+
+templates/
+    home.html
+    index.html
+    chance.html
+    no_chance.html
+
+static/
+    main.css
+    main.js
+
+Flask uses:
+- templates/ for HTML files
+- static/ for CSS and JavaScript files
+""")
